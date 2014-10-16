8854be874286d0189b164ca81c17c5096912c539
M S Vishwanath Bhat <vbhat@redhat.com>
2014-10-16 16:32:52 +0530

    Adding a basic gluster test
    * Creates a gluster volume
    * mounts the gluster volume
    * cleans up the volume
    
    Signed-off-by: M S Vishwanath Bhat <vbhat@redhat.com>

---
+++ tests_d/example/test_basic_gluster_tests.py
@@ -0,0 +1,41 @@
+from gluster_libs.util import testcase, create_volume, mount_volume
+
+@testcase("gluster_basic_test")
+def gluster_basic_test(tc):
+    tc.logger.info("Testing gluster volume create and mounting")
+    ret, _ = tc.run_servers("pgrep glusterd || /etc/init.d/glusterd start")
+    if not ret:
+        tc.logger.error("Unable to start glusterd. Please check the logs")
+        return False
+    volname = 'testvol'
+    mnode = tc.nodes[0]
+    mountpoint = '/mnt/glusterfs'
+    rc = True
+    for peer in tc.nodes[1:]:
+        ret, out, err  = tc.run(node, "gluster peer probe %s" % peer)
+        if ret != 0:
+            rc = False
+    if not rc:
+        tc.logger.error("Peer probe failed in at least one node. Aborting test")
+        return False
+    ret, out, err = create_volume(volname, 2, 2, trans='tcp')
+    if ret != 0:
+        tc.logger.error("volume create failed")
+        rc = False
+    ret, _, _ = tc.run(mnode, "gluster volume start %s" % volname)
+    if ret[0] != 0:
+        tc.logger.error("Volume start failed")
+        rc = False
+    ret, _, _ = mount_volume(volname, 'glusterfs', mountpoint)
+    if ret != 0:
+        tc.logger.error("mounting volume %s failed" % volname)
+        rc = False
+    if rc:
+        ret, _, _ = tc.run(tc.clients[0], "cp -r /etc %s" % mountpoint)
+        if ret != 0:
+            tc.logger.error("cp failed on the mountpoint")
+            rc = False
+        tc.run(tc.clients[0], "umount %s || umount -l %s" % (mountpoint, mountpoint))
+    tc.run(mnode, "gluster --mode=script volume stop %s" % volname)
+    tc.run(mnode, "gluster --mode=script volume delete %s" % volname)
+    return False
