7af26ee0f02d4c97239ead6e1cd6da82972febd3
M S Vishwanath Bhat <msvbhat@gmail.com>
2015-05-27 21:35:47 +0530

    Adding more volume ops to volume_ops.py
    * get_volume_info - To fetch the volume info in a dict
    * get_volume_status - To fetch the volume status
    * setup_meta_vol - To setup a meta vol for gluster
    
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

--- libs/volume_ops.py
+++ libs/volume_ops.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python
+
 import re
 import time
 from libs.util import tc
-from libs.gluster_init import env_setup_servers, start_glusterd
+from pprint import pformat
 from libs.peer_ops import peer_probe
+from libs.mount_ops import mount_volume
+try:
+    import xml.etree.cElementTree as etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+from libs.quota_ops import enable_quota, set_quota_limit
+from tests_d.uss.uss_libs import set_uss
+from libs.gluster_init import env_setup_servers, start_glusterd
 
 """
     This file contains the gluster volume operations like create volume,
@@ -15,7 +25,6 @@ def create_volume(volname, dist, rep=1, stripe=1, trans='tcp', servers='', \
         Create the gluster volume specified configuration
         volname and distribute count are mandatory argument
     """
-    global tc
     if servers == '':
         servers = tc.nodes[:]
     dist = int(dist)
@@ -113,6 +122,8 @@ def delete_volume(volname, mnode=''):
     """
     if mnode == '':
         mnode = tc.nodes[0]
+    volinfo = get_volume_info(volname, mnode)
+    bricks = volinfo[volname]['bricks']
     ret = tc.run(mnode, "gluster volume delete %s --mode=script" % volname)
     if ret[0] != 0:
         return False
@@ -120,10 +131,59 @@ def delete_volume(volname, mnode=''):
         del tc.global_flag[volname]
     except KeyError:
         pass
+    for brick in bricks:
+        node, vol_dir = brick.split(":")
+        ret = tc.run(node, "rm -rf %s" % vol_dir)
+
     return True
 
 
-def setup_vol(volname='', dist=1, rep=1, dispd=1, red=1, stripe=1, trans='', \
+def setup_meta_vol(servers=''):
+    """
+        Creates, starts and mounts the gluster meta-volume on the servers
+        specified.
+    """
+    if servers == '':
+        servers = tc.nodes
+    meta_volname = 'gluster_shared_storage'
+    mount_point = '/var/run/gluster/shared_storage'
+    metav_dist = int(tc.config_data['META_VOL_DIST_COUNT'])
+    metav_rep = int(tc.config_data['META_VOL_REP_COUNT'])
+    _num_bricks = metav_dist * metav_rep
+    repc = ''
+    if metav_rep > 1:
+        repc = "replica %d" % metav_rep
+    bricks = ''
+    brick_root = "/bricks"
+    _n = 0
+    for i in range(0, _num_bricks):
+        bricks = "%s %s:%s/%s_brick%d" % (bricks, servers[_n], \
+                brick_root, meta_volname, i)
+        if _n < len(servers) - 1:
+            _n = _n + 1
+        else:
+            _n = 0
+    gluster_cmd = "gluster volume create %s %s %s force" \
+            % (meta_volname, repc, bricks)
+    ret = tc.run(servers[0], gluster_cmd)
+    if ret[0] != 0:
+        tc.logger.error("Unable to create meta volume")
+        return False
+    ret = start_volume(meta_volname, servers[0])
+    if not ret:
+        tc.logger.error("Unable to start the meta volume")
+        return False
+    time.sleep(5)
+    for server in servers:
+        ret = mount_volume(meta_volname, 'glusterfs', mount_point, server, \
+                server)
+        if ret[0] != 0:
+            tc.logger.error("Unable to mount meta volume on %s"% server)
+            return False
+    return True
+
+
+def setup_vol(volname='', dist='', rep='', dispd='', red='', stripe='', trans='', \
         servers=''):
     """
         Setup a gluster volume for testing.
@@ -140,15 +200,15 @@ def setup_vol(volname='', dist=1, rep=1, dispd=1, red=1, stripe=1, trans='', \
     """
     if volname == '':
         volname = tc.config_data['VOLNAME']
-    if dist == 1:
+    if dist == '':
         dist = tc.config_data['DIST_COUNT']
-    if rep == 1:
+    if rep == '':
         rep = tc.config_data['REP_COUNT']
-    if dispd == 1:
+    if dispd == '':
         dispd = tc.config_data['DISPERSE']
-    if red == 1:
+    if red == '':
         red = tc.config_data['REDUNDANCY']
-    if stripe == 1:
+    if stripe == '':
         stripe = tc.config_data['STRIPE']
     if trans == '':
         trans = tc.config_data['TRANS_TYPE']
@@ -183,7 +243,18 @@ def setup_vol(volname='', dist=1, rep=1, dispd=1, red=1, stripe=1, trans='', \
     if ret[0] != 0:
         tc.logger.error("Unable to create volume %s" % volname)
         return False
-    time.sleep(4)
+    time.sleep(2)
+    if tc.config_data['ENABLE_QUOTA'] == 'True':
+        ret0 = enable_quota(volname, servers[0])
+        ret1 = set_quota_limit(volname, server=servers[0])
+        if ret0[0] != 0 or ret1[0] != 0:
+            tc.logger.error("Quota setup failed")
+            return False
+    if tc.config_data['ENABLE_USS'] == 'True':
+        ret = set_uss(volname, 'enable', servers[0])
+        if not ret:
+            tc.logger.error("Unable to enable USS for volume %s" % volname)
+            return False
     ret = start_volume(volname, servers[0])
     if not ret:
         tc.logger.error("volume start %s failed" % volname)
@@ -192,37 +263,198 @@ def setup_vol(volname='', dist=1, rep=1, dispd=1, red=1, stripe=1, trans='', \
     return True
 
 
-def get_service_status(volname, services="ALL", servers=''):
+def _parse_volume_status_xml(root_xml):
     """
-        get the service status of each service as displayed by the
-        gluster volume status
+    Helper module for get_volume_status. It takes root xml object as input,
+    parses and returns the 'volume' tag xml object.
     """
-    if servers == '':
-        servers = tc.nodes
-    if services == "ALL":
-        services = ["NFS", "Quota", "Self-heal", "Brick"]
-    else:
-        services = single_element_list(services)
-        servers = single_element_list(servers)
-    for service in services:
-        print service
-        flag = tc.run(servers[0], "gluster vol status %s | grep %s " \
-                % (volname, service))
-        if flag[0] == 0:
-            flag = tc.run(servers[0], "gluster vol status %s | grep %s | egrep \
-'[[:space:]]+N[[:space:]]+'" % (volname, service))
-            if flag[0] == 0:
-                for server in servers:
-                    if service != "Brick" and server == servers[0]:
-                        server = "localhost"
-                    if re.search(server, flag[1]):
-                        return False
-        # If service is not listed in vol status return false
-        else:
-            return False
-    return True
+    for element in root_xml:
+        if element.findall("volume"):
+            return element.findall("volume")
+        root_vol = _parse_volume_status_xml(element)
+        if root_vol is not None:
+            return root_vol
 
-def single_element_list(input):
-    if not isinstance(input, list):
-        input = [input]
-    return input
+def parse_xml(tag_obj):
+    """
+    This module takes any xml element object and parses all the child nodes
+    and returns the parsed data in dictionary format
+    """
+    node_dict = {}
+    for tag in tag_obj:
+        if re.search(r'\n\s+', tag.text) is not None:
+            port_dict = {}
+            port_dict = parse_xml(tag)
+            node_dict[tag.tag] = port_dict
+        else:
+            node_dict[tag.tag] = tag.text
+    return node_dict
+
+def get_volume_status(server='', volname='all', service='', options=''):
+    """
+    This module gets the status of all or specified volume(s)/brick
+    @parameter:
+        * server  - <str> (optional) name of the server to execute the volume
+                    status command. If not given, the function takes the
+                    first node from config file
+        * volname - <str> (optional) name of the volume to get status. It not
+                    given, the function returns the status of all volumes
+        * service - <str> (optional) name of the service to get status.
+                    serivce can be, [nfs|shd|<BRICK>|quotad]], If not given,
+                    the function returns all the services
+        * options - <str> (optional) options can be,
+                    [detail|clients|mem|inode|fd|callpool|tasks]. If not given,
+                    the function returns the output of gluster volume status
+    @Returns: volume status in dict of dictionary format, on success
+              None, on failure
+    """
+
+    if server == '':
+        server = tc.nodes[0]
+
+    cmd = "gluster vol status %s %s %s --xml" % (volname, service, options)
+
+    ret = tc.run(server, cmd)
+    if ret[0] != 0:
+        tc.logger.error("Failed to execute gluster volume status command")
+        return None
+
+    root = etree.XML(ret[1])
+    volume_list = _parse_volume_status_xml(root)
+    if volume_list is None:
+        tc.logger.error("No volumes exists in the gluster")
+        return None
+
+    vol_status = {}
+    for volume in volume_list:
+        tmp_dict1 = {}
+        tmp_dict2 = {}
+        vol_name = [vol.text for vol in volume if vol.tag == "volName"]
+
+        # parsing volume status xml output
+        if options == 'tasks':
+            tasks = volume.findall("tasks")
+            for each_task in tasks:
+                tmp_dict3 = parse_xml(each_task)
+                node_name = 'task_status'
+                if 'task' in tmp_dict3.keys():
+                    if node_name in tmp_dict2.keys():
+                        tmp_dict2[node_name].append(tmp_dict3['task'])
+                    else:
+                        tmp_dict2[node_name] = [tmp_dict3['task']]
+                else:
+                    tmp_dict2[node_name] = [tmp_dict3]
+        else:
+            nodes = volume.findall("node")
+            for each_node in nodes:
+                if each_node.find('path').text.startswith('/'):
+                    node_name = each_node.find('hostname').text
+                else:
+                    node_name = each_node.find('path').text
+                node_dict = parse_xml(each_node)
+                tmp_dict3 = {}
+                if "hostname" in node_dict.keys():
+                    if node_dict['path'].startswith('/'):
+                        tmp = node_dict["path"]
+                        tmp_dict3[node_dict["path"]] = node_dict
+                    else:
+                        tmp_dict3[node_dict["hostname"]] = node_dict
+                        tmp = node_dict["hostname"]
+                    del tmp_dict3[tmp]["path"]
+                    del tmp_dict3[tmp]["hostname"]
+
+                if node_name in tmp_dict1.keys():
+                    tmp_dict1[node_name].append(tmp_dict3)
+                else:
+                    tmp_dict1[node_name] = [tmp_dict3]
+
+                tmp_dict4 = {}
+                for item in tmp_dict1[node_name]:
+                    for key, val in item.items():
+                        tmp_dict4[key] = val
+                tmp_dict2[node_name] = tmp_dict4
+
+        vol_status[vol_name[0]] = tmp_dict2
+    tc.logger.debug("Volume status output: %s" \
+                    % pformat(vol_status, indent=10))
+    return vol_status
+
+def get_volume_option(volname, option='all', server=''):
+    """
+    This module gets the option values for the given volume.
+    @parameter:
+        * volname - <str> name of the volume to get status.
+        * option  - <str> (optional) name of the volume option to get status.
+                    If not given, the function returns all the options for
+                    the given volume
+        * server  - <str> (optional) name of the server to execute the volume
+                    status command. If not given, the function takes the
+                    first node from config file
+    @Returns: value for the given volume option in dict format, on success
+              None, on failure
+    """
+    if server == '':
+        server = tc.nodes[0]
+
+    cmd = "gluster volume get %s %s" % (volname, option)
+    ret = tc.run(server, cmd)
+    if ret[0] != 0:
+        tc.logger.error("Failed to execute gluster volume get command")
+        return None
+
+    volume_option = {}
+    raw_output = ret[1].split("\n")
+    for line in raw_output[2:-1]:
+        match = re.search(r'^(\S+)(.*)', line.strip())
+        if match is None:
+            tc.logger.error("gluster get volume output is not in \
+                             expected format")
+            return None
+
+        volume_option[match.group(1)] = match.group(2).strip()
+
+    return volume_option
+
+
+def get_volume_info(volname='all', server=''):
+    """
+        Fetches the volume information as displayed in the volume info.
+        Uses xml output of volume info and parses the into to a dict
+
+        Returns a dict of dicts.
+        -- Volume name is the first key
+        -- distCount/replicaCount/Type etc are second keys
+        -- The value of the each second level dict depends on the key
+        -- For distCount/replicaCount etc the value is key
+        -- For bricks, the value is a list of bricks (hostname:/brick_path)
+    """
+    if server == '':
+        server = tc.nodes[0]
+    ret = tc.run(server, "gluster volume info %s --xml" % volname, \
+            verbose=False)
+    if ret[0] != 0:
+        tc.logger.error("volume info returned error")
+        return None
+    root = etree.XML(ret[1])
+    volinfo = {}
+    for volume in root.findall("volInfo/volumes/volume"):
+        for elem in volume.getchildren():
+            if elem.tag == "name":
+                volname = elem.text
+                volinfo[volname] = {}
+            elif elem.tag == "bricks":
+                volinfo[volname]["bricks"] = []
+                for el in elem.getiterator():
+                    if el.tag == "name":
+                        volinfo[volname]["bricks"].append(el.text)
+            elif elem.tag == "options":
+                volinfo[volname]["options"] = {}
+                for option in elem.findall("option"):
+                    for el in option.getchildren():
+                        if el.tag == "name":
+                            opt = el.text
+                        if el.tag == "value":
+                            volinfo[volname]["options"][opt] = el.text
+            else:
+                volinfo[volname][elem.tag] = elem.text
+    return volinfo
