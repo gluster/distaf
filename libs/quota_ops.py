2f45a5bc9b1fa6ea78fd3ddf93ec7f60f09fb425
M S Vishwanath Bhat <msvbhat@gmail.com>
2015-05-29 15:04:15 +0530

    Adding quota_ops to libs
    Contains functions to enable quota and set the limit-usage on
    a path of a volume with specified limit
    
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

---
+++ libs/quota_ops.py
@@ -0,0 +1,25 @@
+from libs.util import tc
+
+def enable_quota(volname, server=''):
+    """
+        Enables quota on the specified volume
+
+        Returns the output of quota cli command
+    """
+    if server == '':
+        server = tc.nodes[0]
+    cmd = "gluster volume quota %s enable" % volname
+    return tc.run(server, cmd)
+
+
+def set_quota_limit(volname, path='/', limit='100GB', server=''):
+    """
+        Sets limit-usage on the path of the specified volume to
+        specified limit
+
+        Returs the output of quota limit-usage command
+    """
+    if server == '':
+        server = tc.nodes[0]
+    cmd = "gluster volume quota %s limit-usage %s %s" % (volname, path, limit)
+    return tc.run(server, cmd)
