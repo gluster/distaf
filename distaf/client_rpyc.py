b3dea02b3e45d4042e7e61639e83cbdcbd31078f
M S Vishwanath Bhat <msvbhat@gmail.com>
2015-09-28 18:41:32 +0530

    Changing the config.sh to config.yml
    This patch removes the pseudo config file config.sh and
    intoduces a new config file in yaml format
    
    The new yaml config also introduces a new structure of
    specifying the configuration variable during runtime.
    
    * servers section enunciates all the servers.
      Since gluster tests machines broadly come under either
      servers or clients. Just specify all your gluster servers
      here. The segregation of servers into nodes and peers should
      happen in the volume section.
    
      Also devices needs to be specified. If devices field is empty
      all available disks will be used for creating gluster bricks.
      But this part has not been implemented yet.
    
    * clients section enunciates all the clients.
    
    * volume section enunciates all the volume which needs to be
      created. All parameters of the volume, like name, dist and rep
      count etc will be specified under this section. You also have
      to specify on which servers this volume should be created.
      In ideal scenario, these should be mentioned in the servers
      section. If not they will be added to the section anyway
    
    * geo-rep section is for geo-rep tests
    
    Signed-off-by: M S Vishwanath Bhat <msvbhat@gmail.com>

--- distaf/client_rpyc.py
+++ distaf/client_rpyc.py
@@ -13,33 +13,22 @@ class big_bang:
         """
         self.config_data = get_config_data()
 
-        self.nodes = self.config_data['NODES']
-        self.peers = self.config_data['PEERS']
-        self.clients = self.config_data['CLIENTS']
-        self.gm_nodes = self.config_data['GM_NODES']
-        self.gm_peers = self.config_data['GM_PEERS']
-        self.gs_nodes = self.config_data['GS_NODES']
-        self.gs_peers = self.config_data['GS_PEERS']
-        self.number_nodes = len(self.nodes)
-        self.number_peers = len(self.peers)
-        self.number_clients = len(self.clients)
-        self.number_gm_nodes = len(self.gm_nodes)
-        self.number_gm_peers = len(self.gm_peers)
-        self.number_gs_nodes = len(self.gs_nodes)
-        self.number_gs_peers = len(self.gs_peers)
+        # Initialise servers and clients
+        self.servers = self.config_data['servers'].keys()
+        self.clients = self.config_data['clients'].keys()
+        self.all_nodes = list(set(self.servers + self.clients))
+        self.num_servers = len(self.servers)
+        self.num_clients = len(self.clients)
+        self.user = self.config_data['remote_user']
         self.global_flag = {}
 
-        self.servers = self.nodes + self.peers + self.gm_nodes + \
-                self.gm_peers + self.gs_nodes + self.gs_peers
-        self.all_nodes = self.nodes + self.peers + self.clients + \
-                self.gm_nodes + self.gm_peers + self.gs_nodes + self.gs_peers
-
-        client_logfile = self.config_data['LOG_FILE']
-        loglevel = getattr(logging, self.config_data['LOG_LEVEL'].upper())
+        # setup logging in distaf
+        client_logfile = os.path.abspath(self.config_data['log_file'])
+        loglevel = getattr(logging, self.config_data['log_level'].upper())
         client_logdir = os.path.dirname(client_logfile)
         if not os.path.exists(client_logdir):
             os.makedirs(client_logdir)
-        self.logger = logging.getLogger('client_rpyc')
+        self.logger = logging.getLogger('distaf')
         self.lhndlr = logging.FileHandler(client_logfile)
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s '
                                      '%(message)s')
@@ -47,8 +36,7 @@ class big_bang:
         self.logger.addHandler(self.lhndlr)
         self.logger.setLevel(loglevel)
 
-        self.user = self.config_data['REMOTE_USER']
-
+        # Make connections
         self.connection_handles = {}
         self.subp_conn = {}
         for node in self.all_nodes:
